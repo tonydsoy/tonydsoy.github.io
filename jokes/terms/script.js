f4b6d9860ae7fec41d6b6e669651280b3c1f2969
tonydsas <169089114+tonydsas@users.noreply.github.com>
2025-01-10 16:57:21 +0000

    modified:   jokes/terms/script.js

--- jokes/terms/script.js
+++ jokes/terms/script.js
@@ -3,7 +3,7 @@ var skbidi = document.getElementById("skibidi")
 var title = document.getElementById("title")
 
 function audios() {
-    skbidi.innerHTML = "<h2>1. </h2> <img type='image', src='kitty.jpeg'></img><br><h1>top 10 ways to avoid autoplay blocking</h1>"
+    skbidi.innerHTML = "<h2>1. </h2> <img type='image', src='kitty.jpeg'></img><br><h1>i ain't writing a TOS</h1>"
     title.innerHTML = "tonyweb - gotcha!"
     audio.play()
 }
