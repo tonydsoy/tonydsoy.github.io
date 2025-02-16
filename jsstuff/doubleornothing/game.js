21c09a186cd360145ba636a5d0da0ac7d326f1d1
tonydsoy <97201792+tonydsoy@users.noreply.github.com>
2025-02-16 01:46:31 +0000

    unfinished version of doubleornothing

---
+++ jsstuff/doubleornothing/game.js
@@ -0,0 +1,27 @@
+const money = document.getElementById("money").innerHTML
+const forjs = document.getElementById("forjs").innerHTML
+
+function event(i, v) {
+    if (i==0) { if (v==0) {
+        // black
+        
+    }}
+    if (i==1) { if (v==0) {
+        // red
+        
+    }}
+    if (v==1) {
+        // ended black
+
+    }
+    if (v==2) {
+        // ended red
+
+    }
+}
+
+function blip() {
+    // blackred <div id="pingpong" class="pingpong"><div class="pingblack"></div><div class="pingred"></div></div>
+    // redblack <div id="pingpong" class="pingpong"><div class="pingred"></div><div class="pingblack"></div></div>
+
+}
