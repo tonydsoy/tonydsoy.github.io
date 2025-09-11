18ca5be11ae2f2558b8b66de64d4f3e715b762a4
tonydsas <169089114+tonydsas@users.noreply.github.com>
2025-09-11 18:04:09 -0400

    Update script.js

--- jokes/terms/script.js
+++ jokes/terms/script.js
@@ -3,7 +3,8 @@ var skbidi = document.getElementById("skibidi")
 var title = document.getElementById("title")
 
 function audios() {
-    skbidi.innerHTML = "<h2>1. </h2> <img type='image', src='kitty.jpeg'></img><br><h1>i ain't writing a TOS</h1>"
+    skbidi.innerHTML = "<h2>1. </h2> <img type='image', src='kitty.jpeg'></img><br><h1>laugh</h1>"
     title.innerHTML = "tonyweb - gotcha!"
     audio.play()
+
 }
