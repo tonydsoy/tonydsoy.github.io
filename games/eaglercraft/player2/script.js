16aefb0668fc49d0399a3b453afb082d0c291b1f
tonydsoy <anthonykarczewski1@outlook.com>
2025-09-25 20:24:37 -0400

    updated VSNF to version 2

--- games/eaglercraft/player2/script.js
+++ games/eaglercraft/player2/script.js
@@ -1,15 +1,21 @@
 const drops = document.getElementById("drops")
 const versionSelect = document.getElementById("versionSelect")
-const SelectedVersion = "eagler12"
+let SelectedVersion = "eagler12"
 
 // init
 window.addEventListener("DOMContentLoaded", function() {
     for (const i of drops.children) {
-        i.addEventListener("click",function() {
+        i.addEventListener("click", function() {
             const EaglerVerison = i.getAttribute("eagler")
             console.log(EaglerVerison)
             SelectedVersion = EaglerVerison
-            versionSelect.innerHTML = i.innerHTML
+            versionSelect.textContent = i.textContent
         })
     }
 })
+
+const play = document.getElementById("play")
+
+play.addEventListener("click", function() {
+    window.location.href = `${window.location.pathname}/${SelectedVersion}`;
+});
