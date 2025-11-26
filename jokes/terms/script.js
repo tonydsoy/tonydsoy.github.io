88d45346c7b2121b546001446f2c1b21478bced3
tonydsas <169089114+tonydsas@users.noreply.github.com>
2025-11-26 11:39:33 -0500

    should probably change this

--- jokes/terms/script.js
+++ jokes/terms/script.js
@@ -3,8 +3,9 @@ var skbidi = document.getElementById("skibidi")
 var title = document.getElementById("title")
 
 function audios() {
-    skbidi.innerHTML = "<h2>1. </h2> <img type='image', src='kitty.jpeg'></img><br><h1>laugh</h1>"
+    skbidi.innerHTML = "<h2>1. </h2> <img type='image', src='kitty.jpeg'></img><br><h1>you have to laugh</h1>"
     title.innerHTML = "tonyweb - gotcha!"
     audio.play()
 
 }
+
