cf5755fb176b12edcb653a81d350668ed7051e9c
tonydsas <169089114+tonydsas@users.noreply.github.com>
2026-02-12 19:59:41 +0000

    removed fr-fr and siterds

--- fr-fr/jokes/terms/script.js
+++
@@ -1,9 +0,0 @@
-var audio = document.getElementById("audio")
-var skbidi = document.getElementById("skibidi")
-var title = document.getElementById("title")
-
-function audios() {
-    skbidi.innerHTML = "<h2>1. </h2> <img type='image', src='kitty.jpeg'></img><br><h1>je n'écris pas de conditions d'utilisation</h1>"
-    title.innerHTML = "tonyweb - Profitez de votre thé!"
-    audio.play()
-}
