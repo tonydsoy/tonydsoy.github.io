049cc66de18da5f137e16cce3608f39006cafa4f
tonydsoy <anthonykarczewski1@outlook.com>
2026-04-18 09:51:46 -0400

    add neocity

---
+++ neocity/contents.js
@@ -0,0 +1,29 @@
+function changecontent(changeto) {
+    console.log("loading content by name: " + changeto)
+    fetch("contents/" + changeto + ".html").then(res => {
+        if (!res.ok) {
+            document.getElementById("main-content").innerHTML = "Could not find content by that name"
+            throw new Error("content not found: " + res.status);
+        }
+        return res.text();
+    }).then(html => {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(html, "text/html");
+
+        const content = doc.querySelector("#main-content");
+        document.getElementById("main-content").innerHTML = content.innerHTML;
+        const url = new URL(window.location);
+        url.searchParams.set("content",changeto);
+
+        history.pushState({}, "", url);
+    })
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    const urlsearchparams = new URLSearchParams(window.location.search);
+    if (urlsearchparams.get("content") != undefined) {
+        changecontent(urlsearchparams.get("content"));
+    } else {
+        changecontent("homepage");
+    }
+})
