48b77f0fa5b66d4302b78d2d98ba825c683d492a
tonydsoy <anthonykarczewski1@outlook.com>
2026-06-21 15:21:58 -0400

    rename modifiers script

---
+++ modifiers.js
@@ -0,0 +1,42 @@
+window.invertcolors = {};
+invertcolors.inverted = false;
+
+invertcolors.invertReminder = function () {
+    if (invertcolors.inverted == true) {
+        document.querySelector(".middled").style.filter = "invert(1)";
+        document.getElementById("bb-invert").style.backdropFilter = "invert(1)";
+        document.getElementById("modifiers").style.filter = "invert(1)";
+    } else {
+        document.querySelector(".middled").style.filter = "invert(0)";
+        document.getElementById("bb-invert").style.backdropFilter = "invert(0)";
+        document.getElementById("modifiers").style.filter = "invert(0)";
+    }
+
+}
+
+invertcolors.invert = function () {
+    if (invertcolors.inverted == false) {
+        document.cookie = "inverted=true";
+        invertcolors.inverted = true;
+        invertcolors.invertReminder();
+    } else {
+        document.cookie = "inverted=false";
+        invertcolors.inverted = false;
+        invertcolors.invertReminder();
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    if (getCookie("inverted") != undefined) {
+        const inverted = getCookie("inverted");
+        if (inverted == "false") {
+            invertcolors.inverted = false;
+        } else {
+            invertcolors.inverted = true;
+        }
+    } else {
+        invertcolors.inverted = false;
+        document.cookie = "inverted=false";
+    }
+    invertcolors.invertReminder();
+})
