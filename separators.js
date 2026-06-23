3b8441ee565d4e7b24dea328e32bbf56270eb73f
tonydsoy <anthonykarczewski1@outlook.com>
2026-06-23 16:41:13 -0400

    sidebar now starts closed, new sidebar styles!!

--- separators.js
+++ separators.js
@@ -1,23 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let sidebarSections = 0;
-    for (; ;) {
-        const sidebarSeparator = document.getElementById("separator-" + sidebarSections);
-        if (sidebarSeparator != undefined) {
-            sidebarSeparator.addEventListener("click", function () {
-                let sepid = sidebarSeparator.getAttribute("sepid");
-                if (!sidebarSeparator.classList.contains("sidebar-separator-closed")) {
-                    // close
-                    sidebarSeparator.classList.add("sidebar-separator-closed");
-                    document.getElementById("separator-" + sepid + "-sec").style.display = "none";
-                } else {
-                    // open
-                    sidebarSeparator.classList.remove("sidebar-separator-closed");
-                    document.getElementById("separator-" + sepid + "-sec").style.display = "block";
-                }
-            })
-        } else {
-            break;
-        }
-        sidebarSections += 1;
+    const sbs = document.getElementsByClassName("sidebar-separator");
+    for (const sidebarseparator of sbs) {
+        const neighbour = sidebarseparator.nextElementSibling;
+        sidebarseparator.classList.add("sidebar-separator-closed");
+        neighbour.style.display = "none"
+        sidebarseparator.addEventListener("click", () => {
+            if (sidebarseparator.classList.contains("sidebar-separator-closed")) {
+                sidebarseparator.classList.remove("sidebar-separator-closed");
+                neighbour.style.display = "block"
+            } else {
+                sidebarseparator.classList.add("sidebar-separator-closed");
+                neighbour.style.display = "none";
+            }
+        })
     }
 })
