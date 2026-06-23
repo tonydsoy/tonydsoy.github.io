0b43557edc3560c31dd5b99a1eef6b51610eedff
tonydsoy <anthonykarczewski1@outlook.com>
2026-06-23 15:59:48 -0400

    devsense now shows commit SHA, aboutme is now in 'no particular order'

--- devsense.js
+++ devsense.js
@@ -1,8 +1,16 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
     if (window.location.href.startsWith("http://127.0.0.1:3000")) {
         console.log("dev welcome");
         const bg = document.getElementsByClassName("body-background")[0];
-        bg.insertAdjacentHTML("afterbegin", "<span id='devwarn' style='position:fixed;display:block;'>developer</span>");
+
+        const res = await fetch(
+            'https://api.github.com/repos/tonydsoy/tonydsoy.github.io/commits/main'
+        );
+
+        const commit = await res.json();
+        const ncommit = commit.sha.substring(0, 7);
+
+        bg.insertAdjacentHTML("afterbegin", `<span id='devwarn' style='position:fixed;display:block;'>commit: ${ncommit}</span>`);
         document.getElementById("devwarn").addEventListener("click", () => {
             document.getElementById("devwarn").remove();
         })
