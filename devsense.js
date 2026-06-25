cc10e94518016d022ac89f8a59a574797c7e905b
tonydsoy <anthonykarczewski1@outlook.com>
2026-06-25 14:06:38 -0400

    devsense now supports offline development

--- devsense.js
+++ devsense.js
@@ -1,8 +1,8 @@
+// npx serve for some reason includes the .git directory so like.
 window.developer = {}
-developer.enabled = false;
 
-document.addEventListener("DOMContentLoaded", async () => {
-    if (window.location.href.startsWith("http://127.0.0.1:3000")) {
+developer.enableDev = async function () {
+    if (!window.location.href.startsWith("http://127")) {
         console.log("dev welcome");
         const bg = document.getElementsByClassName("body-background")[0];
 
@@ -14,5 +14,22 @@ document.addEventListener("DOMContentLoaded", async () => {
         const ncommit = commit.sha.substring(0, 7);
 
         bg.insertAdjacentHTML("afterbegin", `<span id='devwarn' style='position:fixed;display:block;'>commit: ${ncommit}</span>`);
+    } else {
+        console.log("dev welcome");
+        const bg = document.getElementsByClassName("body-background")[0];
+
+        const res = await fetch('/.git/refs/heads/main');
+
+        let commit = await res.text();
+        commit = commit.substring(0, 7);
+        bg.insertAdjacentHTML("afterbegin", `<span id='devwarn' style='position:fixed;display:block;'>commit: ${commit}</span>`)
+    }
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+    if (window.location.href.startsWith("http://127")) {
+        developer.enableDev();
     }
 })
+
+
