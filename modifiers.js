65931c50ebf5e2c3fff00067c5159afc386e3056
tonydsoy <anthonykarczewski1@outlook.com>
2026-06-25 11:06:55 -0400

    barrelroll merged into modifiers.js

--- modifiers.js
+++ modifiers.js
@@ -41,3 +41,18 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     invertcolors.invertReminder();
 })
+
+// barrelroll
+window.barrelroll = {}
+
+barrelroll.roll = function () {
+    console.log("barrel roll");
+    const body = document.getElementsByTagName("body")[0];
+    body.style.animation = "barrelroll";
+    body.style.animationDuration = "1s";
+    body.style.animationIterationCount = "1";
+    body.style.animationTimingFunction = "linear";
+    setTimeout(() => {
+        body.style.animation = "none";
+    }, 1000)
+}
