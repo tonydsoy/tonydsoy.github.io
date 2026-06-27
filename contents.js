7699b6243669eea19ad03bff4c709f7390038190
tonydsoy <anthonykarczewski1@outlook.com>
2026-06-27 14:50:34 -0400

    popups absorbed into contents.js, music player now has title bar

--- contents.js
+++ contents.js
@@ -18,6 +18,38 @@ function runScripts(container) {
     });
 }
 
+function afterloadcontents(container, actions) {
+    // popups
+    popupactions = container.getElementsByClassName("popup-action");
+
+    for (let button of popupactions) {
+        button.addEventListener("click", () => {
+            const body = document.getElementsByTagName("body")[0];
+            body.insertAdjacentHTML(
+                "afterbegin",
+                "<div class='popup-bg' id='popup-bg'><img id='popup-close' src='graphics/cancel.png'><div id='popup-content'></div></div>"
+            );
+            if (invertcolors.inverted == true) {
+                document.getElementById("popup-bg").style.backdropFilter = "invert(1)";
+                document.getElementById("popup-bg").style.filter = "invert(1)";
+            }
+
+            let popuptype = "none";
+            let popupdata = "none";
+            for (const attr of button.attributes) {
+                if (attr.name == "popup-type") popuptype = attr.value;
+                if (attr.name == "popup-data") popupdata = attr.value;
+            }
+            if (popuptype == "data") {
+                document.getElementById("popup-content").innerHTML = popupdata;
+            }
+            document.getElementById("popup-close").addEventListener("click", () => {
+                document.getElementById("popup-bg").remove();
+            })
+        })
+    }
+}
+
 function fixhref(container) {
     const hrefs = container.querySelectorAll("a");
     hrefs.forEach(href => {
@@ -56,6 +88,8 @@ function changecontent(changeto) {
         url.searchParams.set("c", changeto);
 
         history.pushState({}, "", url);
+
+        afterloadcontents(target);
     })
 }
 
