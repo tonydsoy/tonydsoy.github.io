81c465d493ca0fa7bc25eb24b7d3d44a9233cfa8
tonydsoy <anthonykarczewski1@outlook.com>
2026-07-04 13:53:55 -0400

    musicplayer now starts autoplay as disabled

--- musicplayer.js
+++ musicplayer.js
@@ -57,7 +57,7 @@ document.addEventListener("DOMContentLoaded", () => {
     })
 
     if (getCookie("musicplayer.autoplay") == undefined) {
-        document.cookie = "musicplayer.autoplay=true";
+        document.cookie = "musicplayer.autoplay=false";
     }
 
     if (getCookie("musicplayer.autoplay") == "true") {
